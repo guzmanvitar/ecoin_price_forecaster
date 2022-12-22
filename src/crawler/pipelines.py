d13ac31c36be1d00df268d97c971fb53eca37f28
guzmanvitar <guzmanvitar@gmail.com>
2022-12-22 10:35:14 -0300

    adjust scrapy setting for database dumping

--- src/crawler/pipelines.py
+++ src/crawler/pipelines.py
@@ -1,13 +1,33 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.db_scripts import db_mappings
 
 
-# useful for handling different item types with a single interface
-# from itemadapter import ItemAdapter
+class CoingeckoCrawlerPipeline:
+    CON_STRING = "postgresql://admin:admin@database:5432/postgresdb"
 
+    def __init__(self):
+        self.create_engine()
+        self.create_table()
+        self.define_session()
 
-class CrawlerPipeline:
+    def create_engine(self):
+        self.engine = create_engine(self.CON_STRING)
+
+    def create_table(self):
+        db_mappings.Base.metadata.create_all(self.engine)
+
+    def define_session(self):
+        self.Session = sessionmaker(bind=self.engine)
+
     def process_item(self, item, spider):
+        self.putitemsintable(item)
         return item
+
+    def putitemsintable(self, item):
+        with self.Session() as my_session:
+            my_session.begin()
+            # Use merge to update registry if repeated
+            my_session.merge(db_mappings.Coingecko(**dict(item)))
+            my_session.commit()
