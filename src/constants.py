d3abd6f2cfedf31f898c184922bfc5f24445450b
guzmanvitar <guzmanvitar@gmail.com>
2022-12-24 15:08:13 -0300

    add twitter credentials as secrets

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 this = Path(__file__)
@@ -8,14 +9,21 @@ LOGS = ROOT / "logs"
 
 DATA = ROOT / "data"
 
+SECRETS = ROOT / ".secrets"
+
 DATA_RAW = DATA / "raw"
 DATA_READY = DATA / "ready"
 DATA_INTERIM = DATA / "interim"
+DATA_TWITTER = DATA_RAW / "twitter"
 
-DATA_RAW.mkdir(exist_ok=True, parents=True)
 DATA_READY.mkdir(exist_ok=True, parents=True)
 DATA_INTERIM.mkdir(exist_ok=True, parents=True)
+DATA_TWITTER.mkdir(exist_ok=True, parents=True)
 
 POSTGRESDB_CON_STRING = "postgresql://admin:admin@database:5432/postgresdb"
 
 API_DATE_FORMAT = "%d-%m-%Y"
+
+# TODO: Improve this
+with open(SECRETS / "twitter_credentials.json") as f:
+    TWITTER_CREDENTIALS = json.load(f)
