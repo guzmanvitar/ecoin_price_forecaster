59c5a568730638ec44396ce779a1559bbf1d1712
guzmanvitar <guzmanvitar@gmail.com>
2023-05-22 22:14:50 +0200

    Implement base ARIMA forecaster

--- src/constants.py
+++ src/constants.py
@@ -20,10 +20,15 @@ DATA_INTERIM = DATA / "interim"
 DATA_COINGECKO = DATA_RAW / "coingecko"
 DATA_TWITTER = DATA_RAW / "twitter"
 
+MODELS = ROOT / "models"
+MODELS_FORECASTING = MODELS / "forecasting"
+MODELS_FORECASTING_HISTORY = MODELS_FORECASTING / "history"
+
 DATA_READY.mkdir(exist_ok=True, parents=True)
 DATA_INTERIM.mkdir(exist_ok=True, parents=True)
 DATA_TWITTER.mkdir(exist_ok=True, parents=True)
 DATA_COINGECKO.mkdir(exist_ok=True, parents=True)
+MODELS_FORECASTING_HISTORY.mkdir(exist_ok=True, parents=True)
 
 # Postgres connection, as defined by sqlalchemy formating, and by user, password and name defined in
 # docker compose service.
@@ -38,3 +43,6 @@ COIN_ID = "coin_id"
 DATE = "date"
 COIN_PRICE = "usd_price"
 FULL_SCRAPE_DATA = "full_response"
+
+# Models
+ARIMA_DEAFULT_ORDER = (30, 1, 30)
